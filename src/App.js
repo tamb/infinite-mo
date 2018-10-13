45a133b5876a3128ecd66c5ef807d40471da2c79
TomSaporito <t.saporito@gmail.com>
2018-10-13 09:54:32 -0400

    added component folders

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { styled } from "styled-components";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
+import Universe from "./components/presentational/Universe";
+import Stars from "./components/presentational/Stars";
+import Cloud from "./components/presentational/Cloud";
+import InfinityLogo from "./components/presentational/InfinityLogo";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Universe>
+          <Stars />
+          <Cloud />
+          <header>
+            <InfinityLogo />
+            <h1>Infinite Mo</h1>
+            <h2>there's only one</h2>
+          </header>
+        </Universe>
+      </Router>
     );
   }
 }
 
 export default App;
+
+// <Route exact path="/" component={Home} />
+// <Route path="/about" component={About} />
+// <Route path="/topics" component={Topics} />
