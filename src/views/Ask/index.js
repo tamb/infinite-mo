04ff503f19747bfa5270538c000ca7f894f850eb
TomSaporito <t.saporito@gmail.com>
2018-10-21 18:56:29 -0400

    added some questions to start with for Ask Mo

--- src/views/Ask/index.js
+++ src/views/Ask/index.js
@@ -2,7 +2,36 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import { shuffle } from "../../utils";
+
 const propTypes = {};
+
+const QUESTIONS_ARR = [
+  {
+    question: "Why are you asking this question again?",
+    choices: null
+  },
+  {
+    question: "What's your sex?",
+    choices: ["Male", "Female"]
+  },
+  {
+    question: "So are you sure want Mo to answer?",
+    choices: ["Yes, I'm sure", "Acutally, maybe not"]
+  },
+  {
+    question: "Before we continue, click this button",
+    choices: ["Just a harmless button"]
+  },
+  {
+    question: "What was the last thing you saw before this question?",
+    choices: null
+  },
+  {
+    question: "What was the last thing you ate?  Just checking",
+    choices: null
+  }
+];
 
 const AskWrapper = styled.div`
   position: fixed;
@@ -39,20 +68,38 @@ class Ask extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      index: 0
+      index: 0,
+      questions: QUESTIONS_ARR
     };
   }
 
   componentDidMount() {
     document.title = "Ask - Infinite Mo";
+    this.setState({ questions: shuffle(this.state.questions) });
   }
 
-  generateQuestion() {
+  generateQuestion(e) {
+    e.preventDefault();
+    const q = this.state.questions[this.state.index];
     return (
-      <label>
-        {this.lines[this.state.index]}
-        <input type="text" value="" />
-      </label>
+      <div>
+        <label>
+          {q.question}
+          {q.choices ? (
+            q.choices.map(c => (
+              <button
+                onClick={() => this.generateQuestion()}
+                type="button"
+                className=""
+              >
+                {c}
+              </button>
+            ))
+          ) : (
+            <input type="text" className="" />
+          )}
+        </label>
+      </div>
     );
   }
 
@@ -63,13 +110,13 @@ class Ask extends Component {
         <p className="ask ask__tagline">He shall answer in kind</p>
         <form
           className="ask ask__form"
-          onSubmit={() => this.generateNewQuestion()}
+          onSubmit={e => this.generateQuestion(e)}
         >
           <label>
             Your query:
             <textarea />
           </label>
-          <button>Submit</button>
+          <button type="submit">Submit</button>
         </form>
       </AskWrapper>
     );
