7f15a35e0f6f89cb951cc9937db6b76619e7390f
pondlebob <t.saporito@gmail.com>
2019-04-13 11:54:52 -0400

    clean up manifest and register site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Universe from "./components/presentational/Universe";
 import Stars from "./components/presentational/Stars";
